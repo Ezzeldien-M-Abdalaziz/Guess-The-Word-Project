e0065fb5eba496aef0fd351c6e37744ebc28ce08
Ezzeldien <ezzeldien.m.abdalziz@gmail.com>
2024-03-27 05:43:17 +0200

    styling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let gameName = "Guess Thhe Word";
+let gameName = "Guess The Word";
 document.title = gameName;
 document.querySelector("h1").innerHTML = gameName;
 document.querySelector("footer").innerHTML = `${gameName} Game Created By Ezzeldien`;
