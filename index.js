49a395af2456194bc2d8dac34442d0e7e3765a2e
Ezzeldien <ezzeldien.m.abdalziz@gmail.com>
2024-04-11 21:55:27 +0200

    .

--- index.js
+++ index.js
@@ -2,3 +2,37 @@ let gameName = "Guess The Word";
 document.title = gameName;
 document.querySelector("h1").innerHTML = gameName;
 document.querySelector("footer").innerHTML = `${gameName} Game Created By Ezzeldien`;
+
+//setting game options
+
+let numberOfTries = 6;
+let numberOfLetters = 6;
+let currentTry = 1;
+
+function generateInput(){
+    const inputsContainer = document.querySelector(".inputs");
+
+    //creat main try
+    for (let i = 1; i <= numberOfTries; i++) {
+        const tryDiv = document.createElement("div");
+        tryDiv.classList.add(`try-${i}`);
+        tryDiv.innerHTML = `<span>Try ${i}</span>`;
+
+        if(i != 1) tryDiv.classList.add("disabled-inputs");
+
+        //create inputs
+        for(let j = 1; j <= numberOfLetters; j++){
+            const input = document.createElement("input");
+            input.type = "text";
+            input.id = `Guess-${i}-Letter-${j}`;
+            input.setAttribute("maxLength","1");
+            tryDiv.appendChild(input);
+        }
+        inputsContainer.appendChild(tryDiv);
+    }
+    inputsContainer.children[0].children[1].focus();
+}
+
+window.onload = function(){
+    generateInput();
+}
