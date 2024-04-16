e048a55619d02aeb4da307b310f8341efb2d2d87
Ezzeldien <ezzeldien.m.abdalziz@gmail.com>
2024-04-16 21:46:24 +0200

    .

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function generateInput(){
         tryDiv.classList.add(`try-${i}`);
         tryDiv.innerHTML = `<span>Try ${i}</span>`;
 
-        if(i != 1) tryDiv.classList.add("disabled-inputs");
+        if(i != 1) tryDiv.classList.add("disabled-inputs");  //remove later
 
         //create inputs
         for(let j = 1; j <= numberOfLetters; j++){
@@ -31,8 +31,25 @@ function generateInput(){
         inputsContainer.appendChild(tryDiv);
     }
     inputsContainer.children[0].children[1].focus();
-}
+
+    //disable all inputs except first one
+    const inputsInDisabledDiv = document.querySelectorAll(".disabled-inputs > input");   //".disabled-inputs > input" because we are selecting the inputs inside the div with this class.
+    inputsInDisabledDiv.forEach((input)=>(input.disabled = true));
+
+    //eventlistner to shift to the second input after writing just one letter
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach((input,index)=> {
+        //convert input to UpperCase
+        input.addEventListener('input',function(e){
+            this.value = this.value.toUpperCase();
+            //console.log(index);
+            const nextInput = inputs[index + 1];
+        });
+    });
+
+} 
 
 window.onload = function(){
     generateInput();
 }
+
